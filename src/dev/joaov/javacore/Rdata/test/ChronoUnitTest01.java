feac85fb276a97ef9a803e9d8326da2ca6dea1a9
joaov-ap <vitor.1943@gmail.com>
2025-04-09 14:48:59 -0300

    feat: datas, resource bundle e regex

---
+++ src/dev/joaov/javacore/Rdata/test/ChronoUnitTest01.java
@@ -0,0 +1,16 @@
+package dev.joaov.javacore.Rdata.test;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class ChronoUnitTest01 {
+    public static void main(String[] args) {
+        LocalDateTime aniversario = LocalDateTime.of(2002, 5, 14, 12, 0);
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(ChronoUnit.DAYS.between(aniversario, now));
+        System.out.println(ChronoUnit.WEEKS.between(aniversario, now));
+        System.out.println(ChronoUnit.MONTHS.between(aniversario, now));
+        System.out.println(ChronoUnit.YEARS.between(aniversario, now));
+    }
+}
