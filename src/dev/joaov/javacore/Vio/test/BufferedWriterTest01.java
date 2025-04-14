48f7509caff7fbceec9ba788582749c470f67cb0
joaov-ap <vitor.1943@gmail.com>
2025-04-14 11:43:16 -0300

    File Reader and Writer

---
+++ src/dev/joaov/javacore/Vio/test/BufferedWriterTest01.java
@@ -0,0 +1,21 @@
+package dev.joaov.javacore.Vio.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw)) {
+
+            bw.write("O DevDojo é lindo, é o melhor curso do brasillllllll");
+            bw.newLine();
+            bw.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/dev/joaov/javacore/Vio/test/FileWriterTest01.java
@@ -0,0 +1,23 @@
+package dev.joaov.javacore.Vio.test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+// File
+// FileWriter
+// FileReader
+// BufferedWriter
+// BufferedReader
+
+public class FileWriterTest01 {
+    public static void main(String[] args) {
+        File file = new File("file.txt");
+        try(FileWriter fw = new FileWriter(file,true)) {
+            fw.write("O DevDojo é lindo, é o melhor curso do brasillllllll\nContinuando a cantoria na próxima linha\n");
+            fw.flush();
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
